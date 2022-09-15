bf9d6171af5715cf611669e8c4eb467dd9cf1bd3
PPAS-12 <xiaoqinglinshi@163.com>
2022-09-15 22:27:54 +0800

    TTT内测包（动态更新中！）

---
+++ common/defines/TTT_Defines.lua
@@ -0,0 +1,7 @@
+NDefines.NBuildings.MAX_SHARED_SLOTS = 35
+NDefines.NFocus.FOCUS_POINT_DAYS = 1 -- Each point takes one day
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 15 -- This much progress can be saved while not having a focus selected
+NDefines.NGame.START_DATE = "1999.4.1.12"
+NDefines.NGame.END_DATE = "2022.8.9.23"
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 10
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 25
---
+++ common/defines/TTT_Graphics.lua
@@ -0,0 +1,7 @@
+NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this amount of VP the map icon becomes bigger dot.
+
+NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 3500.0
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
+
+NDefines_Graphics.NSound.BATTLE_SOUND_MIN_UNIT_COUNT = 4
+NDefines.NGraphics.MAX_NUMBER_OF_TEXTURES = 25000

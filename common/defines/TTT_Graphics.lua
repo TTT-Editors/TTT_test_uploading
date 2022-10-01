4fe181dedeea530153cacceea9ddfc064547199c
PPAS-12 <xiaoqinglinshi@163.com>
2022-10-01 21:56:30 +0800

    TTT-0.6.4.2-20221001-Test

--- common/defines/TTT_Graphics.lua
+++ common/defines/TTT_Graphics.lua
@@ -2,6 +2,4 @@ NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this
 
 NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 3500.0
 NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
-
-NDefines_Graphics.NSound.BATTLE_SOUND_MIN_UNIT_COUNT = 4
 NDefines.NGraphics.MAX_NUMBER_OF_TEXTURES = 25000

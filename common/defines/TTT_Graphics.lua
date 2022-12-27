90f5c57bd381f3d1bb2fe4694300353c4d14eb0d
comrade123456 <114755174+comrade123456@users.noreply.github.com>
2022-12-27 21:39:35 +0800

    加了个制造商

--- common/defines/TTT_Graphics.lua
+++ common/defines/TTT_Graphics.lua
@@ -3,3 +3,4 @@ NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this
 NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 3500.0
 NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
 NDefines.NGraphics.MAX_NUMBER_OF_TEXTURES = 25000
+

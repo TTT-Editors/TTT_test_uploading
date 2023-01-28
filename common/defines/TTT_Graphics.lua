c1352930c8acc2afb4e3ecb6ed0c1f7f50a2d8df
PPAS-12 <xiaoqinglinshi@163.com>
2023-01-29 01:03:21 +0800

    TTT-0.6.6.1-20230129-Alpha

--- common/defines/TTT_Graphics.lua
+++ common/defines/TTT_Graphics.lua
@@ -1,6 +1,1 @@
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_AFTER = {0, 20} -- After this amount of VP the map icon becomes bigger dot.
-
-NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 3500.0
 NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
-NDefines.NGraphics.MAX_NUMBER_OF_TEXTURES = 25000
-
